695b292d9a10c932f11df820a602599ef9d61f60
lindexi <lindexi_gd@163.com>
2024-03-09 16:22:43 +0800

    更新调试代码

--- UnoSpySnoop/Demo/SpySnoopDemo/MainPage.xaml.cs
+++ UnoSpySnoop/Demo/SpySnoopDemo/MainPage.xaml.cs
@@ -5,6 +5,8 @@ public sealed partial class MainPage : Page
     public MainPage()
     {
         this.InitializeComponent();
+#if HAS_UNO
         UnoSpySnoop.SpySnoop.StartSpyUI(SnoopRootGrid);
+#endif
     }
 }
