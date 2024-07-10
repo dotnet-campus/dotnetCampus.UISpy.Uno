0b323c0776d457250c04885652c48e2ca4c5b43c
lindexi <lindexi_gd@163.com>
2024-07-10 20:04:42 +0800

    Revert "尝试去掉包的引用但是失败"
    This reverts commit 6d85a86ec3bb2ff462b9c740925e4d4527e24255.

--- src/dotnetCampus.UISpy.Uno/Models/ElementProxyTreeModel.cs
+++ src/dotnetCampus.UISpy.Uno/Models/ElementProxyTreeModel.cs
@@ -2,18 +2,18 @@ using dotnetCampus.UISpy.Uno.Tree;
 
 namespace dotnetCampus.UISpy.Uno.Models;
 
-//public partial record ElementProxyTreeModel
-//{
-//    public IState<UIElement> CurrentRootElement => State<UIElement>.Empty(this);
+public partial record ElementProxyTreeModel
+{
+    public IState<UIElement> CurrentRootElement => State<UIElement>.Empty(this);
 
-//    public IFeed<ElementProxyTree> CurrentTree => Feed.Async(UpdateVisualTree);
+    public IFeed<ElementProxyTree> CurrentTree => Feed.Async(UpdateVisualTree);
 
-//    public void SetRootElement(UIElement rootElement)
-//    {
-//    }
+    public void SetRootElement(UIElement rootElement)
+    {
+    }
 
-//    private ValueTask<ElementProxyTree> UpdateVisualTree(CancellationToken ct)
-//    {
-//        return ValueTask.FromResult<ElementProxyTree>(default);
-//    }
-//}
+    private ValueTask<ElementProxyTree> UpdateVisualTree(CancellationToken ct)
+    {
+        return ValueTask.FromResult<ElementProxyTree>(default);
+    }
+}
