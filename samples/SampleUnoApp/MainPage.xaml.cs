c74fa03dd31da41abbc5cd6571f0e7e2db16c955
lindexi <lindexi_gd@163.com>
2024-07-10 19:57:20 +0800

    编写测试代码

--- samples/SampleUnoApp/MainPage.xaml.cs
+++ samples/SampleUnoApp/MainPage.xaml.cs
@@ -12,4 +12,9 @@ public sealed partial class MainPage : Page
     }
 
     public dotnetCampus.SampleUnoApp.Localizations.Localizations.ILocalized_Root Lang => dotnetCampus.SampleUnoApp.Localizations.Lang.Current;
+
+    public void Button_OnClick(object sender, RoutedEventArgs args)
+    {
+        this.ShowUnoSpyWindow();
+    }
 }
